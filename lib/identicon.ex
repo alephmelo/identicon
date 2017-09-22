53479f080ac3aa3c8e3db09b48ee46866a309e4d
Aleph Melo <alephmelo@icloud.com>
2017-09-22 00:37:19 -0300

    Add hash function to compute md5 from string.

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,2 +1,11 @@
 defmodule Identicon do
+    def main(input) do
+        input
+        |> hash_input
+    end
+
+    def hash_input(input) do
+        :crypto.hash(:md5, input)
+        |> :binary.bin_to_list        
+    end
 end
